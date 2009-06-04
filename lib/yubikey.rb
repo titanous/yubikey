6cb7457b3c2680da9b2a2a4e1a1c196670bccfd0
Jonathan Rudenberg <jon335@gmail.com>
2009-06-04 17:48:42 -0400

    otp verification

--- lib/yubikey.rb
+++ lib/yubikey.rb
@@ -1,7 +1,11 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'net/http'
+require 'openssl'
+
 require 'yubikey_ext'
 require 'yubikey/hex'
 require 'yubikey/modhex'
 require 'yubikey/otp'
+require 'yubikey/otp_verify'
